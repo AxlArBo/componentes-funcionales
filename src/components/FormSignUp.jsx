8bbb9be76956f0c052492717e693c17a444c1968
Axel <axel.man94gmail.com>
2023-05-17 22:17:41 -0600

    Haciendo validaciones en los elementos del form y preparando el proyecto para produccion

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -9,17 +9,94 @@ function FormSigUp({ handleSubmit }) {
     const [prom, setProm] = useState(true);
     const [nov, setNov] = useState(false);
 
+    const [errors, setErrors] = useState({
+        name: {
+            error: false,
+            message: 'Deben ser al menos 3 caracteres',
+        },
+    });
+
+    const [errorsLN, setErrorsLN] = useState({
+        lastName: {
+            errorLN: false,
+            messageLN: 'Deben ser al menos 5 caracteres',
+        },
+    });
+
+    const [errorsE, setErrorsE] = useState({
+        email: {
+            errorE: false,
+            messageE: 'Ingresa un correo electronico valido',
+        },
+    })
+
+    const validarNombre = (nombre) => {
+        if(nombre.length >= 3){
+            return{ 
+                name: { 
+                    error: false, 
+                    message: ''
+                },
+            }
+        }else{
+            return{ 
+                name: { 
+                    error: true, 
+                    message: 'Deben ser al menos 3 caracteres' 
+                },
+            }
+        }
+    }
+
+    const validarLastName = (apellido) => {
+        if(apellido.length >= 5){
+            return{
+                lastName: {
+                    errorLN: false, 
+                    messageLN: '',
+                },
+            }
+        }else{
+            return{
+                lastName: {
+                    errorLN: true, 
+                    messageLN: 'Deben ser al menos 5 caracteres',
+                },
+            }
+        }
+    }
+
+    const validarEmail = (correo) => {
+        if(correo.length >= 10){
+            return{
+                email: {
+                    errorE: false, 
+                    messageE: '',
+                },
+            }
+        }else{
+            return{
+                email: {
+                    errorE: true, 
+                    messageE: 'Ingresa un correo electronico valido',
+                },
+            }
+        }
+    }
+
     return (
-        <form onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit({
-                name, 
-                lastName, 
-                email, 
-                prom, 
-                nov,
-            })
-        }}>
+        <form 
+            onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmit({
+                    name, 
+                    lastName, 
+                    email, 
+                    prom, 
+                    nov,
+                })
+            }}
+        >
             <TextField 
                 id="name" 
                 label="Nombre" 
@@ -30,6 +107,12 @@ function FormSigUp({ handleSubmit }) {
                     setName(e.target.value)
                 }}
                 value={ name }
+                error={ errors.name.error }
+                helperText={ errors.name.error ? errors.name.message : "" }
+                onBlur={(e) => {
+                    setErrors(
+                        validarNombre(e.target.value)) 
+                }}
             />
             <TextField 
                 id="lastname" 
@@ -37,10 +120,17 @@ function FormSigUp({ handleSubmit }) {
                 variant="outlined" 
                 fullWidth
                 margin="normal"
-                onChange={ (e) => {
+                onChange={(e) => {
                     setLastName(e.target.value);
                 }}
                 value={ lastName }
+                error={ errorsLN.lastName.errorLN }
+                helperText={ errorsLN.lastName.errorLN ? errorsLN.lastName.messageLN : "" }
+                onBlur={(e) => {
+                    setErrorsLN(
+                        validarLastName(e.target.value)
+                    )
+                }}
             />
             <TextField 
                 id="email" 
@@ -51,7 +141,14 @@ function FormSigUp({ handleSubmit }) {
                 onChange={ (e) => {
                     setEmail(e.target.value);
                 }}
-                value={email}
+                value={ email }
+                error={ errorsE.email.errorE }
+                helperText={ errorsE.email.errorE ? errorsE.email.messageE : '' }
+                onBlur={(e) => {
+                    setErrorsE(
+                        validarEmail(e.target.value)
+                    )
+                }}
             />
             <FormGroup>
                 <FormControlLabel 
